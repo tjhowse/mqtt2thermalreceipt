0e2c5dd1b5b71a621a737405b31ee704e139798f
tjhowse <tjhowse@gmail.com>
2021-10-12 23:16:32 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# What?
+
+This listens on MQTT and publishes the result to a thermal printer. It's made to run on an ESP32-based M5Stick-C connected to a QR203 thermal recept printer.
+
+# How?
+Use the arduino IDE.
+Install the latest ESP32 arduino board library.
+Install the Adafruit MQTT-for-esp8266/esp32 and thermal printer libaries.
+
+Populate secrets.h:
+
+const char* ssid = "yes";
+const char* password = "yes";
+
+#define MQTT_HOST "yes"
+#define MQTT_PORT 1883
+#define MQTT_USER "yes"
+#define MQTT_PASS "yes"
+#define MQTT_TOPIC_PREFIX "yes"
---
+++ printer.ino
@@ -0,0 +1,87 @@
+#include "Adafruit_Thermal.h"
+#include "SoftwareSerial.h"
+#include <WiFi.h>
+
+#include "WiFiClientSecure.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include "secrets.h"
+
+#define TX_PIN 26
+#define RX_PIN 36
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, MQTT_HOST, MQTT_PORT, MQTT_USER, MQTT_PASS);
+Adafruit_MQTT_Publish boot = Adafruit_MQTT_Publish(&mqtt, MQTT_TOPIC_PREFIX "/receipt_printer/boot");
+Adafruit_MQTT_Subscribe print_text = Adafruit_MQTT_Subscribe(&mqtt, MQTT_TOPIC_PREFIX "/receipt_printer/print_text");
+
+SoftwareSerial mySerial(RX_PIN, TX_PIN);
+Adafruit_Thermal printer(&mySerial);
+
+void connectWifi() {
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectWifi();
+  mqtt.subscribe(&print_text);
+  MQTT_connect();
+
+  boot.publish("Hi");
+  mySerial.begin(9600);
+  printer.begin();
+
+  printer.setDefault();
+  printer.println("Booted.");
+  printer.feed(2);
+
+  // printer.sleep();
+  // delay(3000L);
+  // printer.wake();
+  // printer.setDefault();
+}
+
+void loop() {
+  MQTT_connect();
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+    if (subscription == &print_text) {
+      printer.println((char *)print_text.lastread);
+    }
+  }
+}
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+
+  Serial.println("MQTT Connected!");
+}
